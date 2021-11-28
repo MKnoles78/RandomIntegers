fa7878f7598cb1ae443aeee9670b98fab62326c3
Mike Knoles <mknoles78@gmail.com>
2021-11-27 19:52:58 -0500

    initial commit

---
+++ src/RandomIntegers.java
@@ -0,0 +1,11 @@
+// Fig. 6.6: RandomIntegers.java
+// Shifted and scaled random integers
+
+public class RandomIntegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
