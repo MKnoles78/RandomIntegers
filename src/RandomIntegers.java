5d9e31900d6d307f2e248b89d367c358441720b6
Mike Knoles <mknoles78@gmail.com>
2021-11-27 19:59:03 -0500

    added new class for SecureRandom, and for loop

--- src/RandomIntegers.java
+++ src/RandomIntegers.java
@@ -1,11 +1,25 @@
+import java.security.SecureRandom;
+
 // Fig. 6.6: RandomIntegers.java
 // Shifted and scaled random integers
 
 public class RandomIntegers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// randomNumbers object will produce secure random numbers
+		SecureRandom randomNumbers = new SecureRandom();
 
+		// loop 20 times
+		for (int counter = 1; counter <= 20; counter++) {
+			// pick random integer from 1 to 6
+			int face = 1 + randomNumbers.nextInt(6);
+
+			System.out.printf("%d ", face); // display generated value
+
+			// if counter is divisible by 5, start a new line of output
+			if (counter % 5 == 0) {
+				System.out.println();
+			}
+		}
 	}
-
 }
